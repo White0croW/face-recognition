8fba299b8a47582aaf202df9a94623963840d314
Mark Klimov <markklimoff@gmail.com>
2025-04-08 06:17:40 +0500

    add tests

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,3 +1,7 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import pytest
 import sqlite3
 from unittest.mock import patch, MagicMock
